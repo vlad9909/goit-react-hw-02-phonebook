51143ba8294215cece664e1650fa4ae45072f4a6
vlad9909 <gva9909@gmail.com>
2023-03-12 20:54:49 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,37 @@
+import React from 'react';
+import ColorPicer from './ColorPicer';
+
+const color = [
+  {
+    label: 'Червоний',
+    color: '#FF0000',
+  },
+  {
+    label: 'Жовтий',
+    color: '#FCFF00',
+  },
+  {
+    label: 'Зелений',
+    color: '#00FF2C',
+  },
+  {
+    label: 'Синій',
+    color: '#001DFF',
+  },
+  {
+    label: 'Рожевий',
+    color: '#FE5050',
+  },
+  {
+    label: 'Фіолетовий',
+    color: '#F250FE',
+  },
+  {
+    label: 'Бірюзовий',
+    color: '#50FEEB',
+  },
+];
+
 export const App = () => {
   return (
     <div
@@ -7,10 +41,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <ColorPicer color={color} />
     </div>
   );
 };
