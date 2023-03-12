05a7a915223541ae3cacc8e0bc910ffb53bb3b88
vlad9909 <gva9909@gmail.com>
2023-03-12 22:47:33 +0200

    devel

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,50 +1,55 @@
 import React from 'react';
-import ColorPicer from './ColorPicer';
+import { nanoid } from 'nanoid';
+import './App.css';
 
-const color = [
-  {
-    label: 'Червоний',
-    color: '#FF0000',
-  },
-  {
-    label: 'Жовтий',
-    color: '#FCFF00',
-  },
-  {
-    label: 'Зелений',
-    color: '#00FF2C',
-  },
-  {
-    label: 'Синій',
-    color: '#001DFF',
-  },
-  {
-    label: 'Рожевий',
-    color: '#FE5050',
-  },
-  {
-    label: 'Фіолетовий',
-    color: '#F250FE',
-  },
-  {
-    label: 'Бірюзовий',
-    color: '#50FEEB',
-  },
-];
+export class App extends React.Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: '',
+  };
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <ColorPicer color={color} />
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="container">
+        <form className="form">
+          <p>Name</p>
+          <input
+            className="input"
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          <p>Number</p>
+          <input
+            className="input"
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+          <button className="btn" type="submit">
+            Add contact
+          </button>
+        </form>
+        <p>Contacts</p>
+        <ul className="list">
+          {this.state.contacts.map(({ id, name, number }) => (
+            <li key={id} className="item">
+              {name}: {number}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
